6ae0dff88f381a290d24f72d566351a3e8c185a7
amiznikov <amiznikov@gmail.com>
2017-12-14 21:56:34 +0530

    add bo + whitelist

--- businnes_overview/index.php
+++ businnes_overview/index.php
@@ -1,11 +1,5 @@
-<? require_once('../header.php');?>
-<div class="second_pages_wrapper contacts_wrapper">
-	<div class="pattern_cycrle_contacts1"></div>
-	<div class="pattern_cycrle_contacts2"></div>
-	<div class="second_pages_content content_container">
-		<div class="second_pages_title_wrapper no_exist_page">
-			<div class="second_pages_title  no_exist_page center_second_page_title">Businness overview is under construction</div>
-		</div>
-	<? require_once('../join_us_form.php');?>
-</div>
-<? require_once('../footer.php');?>
+<?php
+// PHP permanent URL redirection
+header("Location: mercatus_businessoverview.pdf", true, 301);
+exit();
+?>
