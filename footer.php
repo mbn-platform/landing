4aed815c5e82786012f122821453d0b0a96e150f
amiznikov <amiznikov@gmail.com>
2018-01-19 00:05:03 +0530

    comeback landing

--- footer.php
+++ footer.php
@@ -2,36 +2,15 @@
 </div><!-- page wrapper -->
 </div><!-- page wrapper full -->
 <div class="footer_wrapper">
-	<div class="footer_triangles_wrapper">
-		<div class="footer_triangles"></div>	
-	</div>
-	<div class="footer_content_background">
-		<div class="footer_content">
-			<div class="footer_copyright">Mercatus © 2017</div>
-		</div>	
-	</div>
-	
-</div>
-
-<div id="modalinfo" class="modal">
-    <div class="tab-info join_us">
-        <div class="tab-info-inner" id="tab-first">
-            <p class="text-center"><strong>Join whitelist</strong></p>
-            <form action="/email/email.php" id="form_join_us">
-                <div class="join_us_field_wrapper clearfix">
-                    <input type="email" id="join_us_email" class="join_us_field email"  value="" placeholder="enter your e-mail">
-                </div>
-                <div id="join_us_status"  class="text-center">
-                </div>
-                <div class="join_us_submit_wrapper join">
-                    <div class="join_us_submit_wrapper1"></div>
-                    <div class="join_us_submit_wrapper2"></div>
-                    <a href="#modalinfo"  id="join_us_submit" rel="modal:open" class="six_section_btn join_us_submit">Join Whitelist</a>
-                </div>
-            </form>
-        </div>
+    <div class="footer_triangles_wrapper">
+        <div class="footer_triangles"></div>    
     </div>
+    <div class="footer_content_background">
+        <div class="footer_content">
+            <div class="footer_copyright">Mercatus © 2017</div>
+        </div>  
+    </div>
+    
 </div>
-
 </body>
 </html>
