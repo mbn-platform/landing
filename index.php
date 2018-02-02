27b5a019dd39dc2b14cd31758c80837b74c92215
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-02-02 23:10:45 +0600

    added advisor

--- index.php
+++ index.php
@@ -428,6 +428,23 @@ of digital assets'>
                             <a class="social_networks_a fb" href="https://www.facebook.com/alexgfinn" target="_blank"></a>
                         </div>
                     </div>                                    
+                    <div class="our_team_item">
+                        <div class="our_team_img_wrapper">
+                            <img class="our_team_img" src="img/uploads/hans_choi.jpg" alt="" title="">
+                        </div>
+                        <div class="our_team_position">
+                            <div class="our_team_position_text">Advisor</div>
+                        </div>
+                        <div class="our_team_name">
+                            <div class="our_team_name_text">Hans Choi</div>
+                        </div>                      
+                        <div class="our_team_description">
+                            <div class="our_team_description_text">CEO of Openchain<br>Fintech expert<br>Software developer</div>
+                        </div>
+                        <div class="social_networks_wrapper">
+                            <a class="social_networks_a ln" href="https://www.linkedin.com/in/hans-namkyu-choi-407618102" target="_blank"></a>
+                        </div>
+                    </div>   
                 </div>
             </div>
 
