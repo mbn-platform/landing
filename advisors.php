69cde36795985e02b3a742f8b12311c6337c369b
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-02-17 11:52:18 +0300

    team and advisors extracted to separate files for convenience, added advisor, updated team description

---
+++ advisors.php
@@ -0,0 +1,98 @@
+<div class="eight_section_wrapper section_wrapper_js  section" data-section-index="6"  data-anchor="section1">
+    <div class="eight_section_content content_container">
+        <h2 class="sections_title">advisors</h2>
+        <div class="our_team_wrapper">
+            <div class="our_team_string">
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/sls.png" alt="" title="" onclick="window.open('https://bitcointalk.org/index.php?topic=1317448.0')">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Advisor</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Kushed</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text" onclick="window.open('https://bitcointalk.org/index.php?topic=1317448.0')"><span class="clickable">The founder of SaluS (SLS)</span></div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a tw" href="https://twitter.com/Kushed_Crypto" target="_blank"></a>
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/alex_finn.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Advisor</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Alexander Finn</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">CEO of Rosblack Capital<br>Entrepreneur<br>Bitcoin Enthusiast</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a tw" href="https://twitter.com/ag_finn" target="_blank"></a>
+                        <a class="social_networks_a fb" href="https://www.facebook.com/alexgfinn" target="_blank"></a>
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/hans_choi.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Advisor</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Hans Choi</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">OpenChain CEO/Dev<br>Blockchain Developer &amp; Architect<br>Fintech expert</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/hans-namkyu-choi-407618102" target="_blank"></a>
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/yan_stolyar.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Advisor</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Yan Stolyar</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">CEO Telecom<br>IT, project management expert<br>MBA in management</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/yanstolyar/" target="_blank"></a>
+                        <a class="social_networks_a fb" href="https://www.facebook.com/yan.stolyar" target="_blank"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="our_team_string our_team_string2">
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/marie_noel.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Advisor</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Marie-Noel Nsana</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">15 years in the Banking and Financial industry<br>Anti-Money Laundering Certified<br>Bitcoin &amp; Cryptocurrency Professional (C4 cert)</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/marie-noel-nsana-120a433/" target="_blank"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ team.php
@@ -0,0 +1,146 @@
+<div class="seventh_section_wrapper section_wrapper_js section" data-section-index="5">
+    <div class="seventh_section_content content_container">
+        <h2 class="sections_title">our team</h2>
+        <div class="our_team_wrapper">
+            <div class="our_team_string">
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/evgeniy_buev.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">CEO</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Eugene Buev</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Serial entrepreneur<br>Trader of cryptocurrency assets since 2015</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/maxim_khukhro.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">CTO</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Maxim Khuhro</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Machine learning and Java expert, AI researcher. 8 years experience at Intel Corp as a team lead.</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/maximkhukhro/" target="_blank"></a>
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/yurii_gerasimov.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">COO</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Yurii Gerasimov</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Marketing in SxS corporation<br>Launched several business start-ups<br>Cryptoanalyst</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/kirill_romanov.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">CMO</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Kirill Romanov</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Digital-agency founder<br>Serial entrepreneur<br>Marketing since 2009</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/kirill-romanov-4u" target="_blank"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="our_team_string our_team_string2">
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/igor_knyazev.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Backend and Solidity Developer</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Igor Knyazev</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Developed own trading platform. Solidity developer since 2016. Blockchain and Smart Contracts expert.</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/vyacheslav_mychkin.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Frontend and Mobile App Developer</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Vyacheslav Mychkin</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Javascript full stack developer, mobile developer (Android, iOS)</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/sonder_s_lim.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Regional Brand Manager, Asia</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Sonder S. Lim</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Digital Assets Advocate<br>Content Marketing Agency Founder<br>Content Strategist</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/sonderslim/" target="_blank"></a>
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/adam_joseph.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Regional Brand Manager, India</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Adam Joseph</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Strategic Marketing<br>Business Development Manager</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/adam-joseph-266356153/" target="_blank"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="sections_arrow_wrapper">
+            <div class="section_arrow"></div>
+            </div> -->
+    </div>
+</div>
