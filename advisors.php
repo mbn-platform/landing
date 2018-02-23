44291fbd8d6298bf287ff8892f2aced07f7d54fa
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-02-23 14:11:16 +0300

    updated docs

--- advisors.php
+++ advisors.php
@@ -5,37 +5,19 @@
             <div class="our_team_string">
                 <div class="our_team_item">
                     <div class="our_team_img_wrapper">
-                        <img class="our_team_img" src="img/uploads/sls.png" alt="" title="" onclick="window.open('https://bitcointalk.org/index.php?topic=1317448.0')">
+                        <img class="our_team_img" src="img/uploads/marie_noel.jpg" alt="" title="">
                     </div>
                     <div class="our_team_position">
-                        <div class="our_team_position_text">Advisor</div>
+                        <div class="our_team_position_text">Financial Advisor</div>
                     </div>
                     <div class="our_team_name">
-                        <div class="our_team_name_text">Kushed</div>
+                        <div class="our_team_name_text">Marie-Noel Nsana</div>
                     </div>
                     <div class="our_team_description">
-                        <div class="our_team_description_text" onclick="window.open('https://bitcointalk.org/index.php?topic=1317448.0')"><span class="clickable">The founder of SaluS (SLS)</span></div>
+                        <div class="our_team_description_text">15 years in the Banking and Financial industry<br>Anti-Money Laundering Certified<br>Bitcoin &amp; Cryptocurrency Professional (C4 cert)</div>
                     </div>
                     <div class="social_networks_wrapper">
-                        <a class="social_networks_a tw" href="https://twitter.com/Kushed_Crypto" target="_blank"></a>
-                    </div>
-                </div>
-                <div class="our_team_item">
-                    <div class="our_team_img_wrapper">
-                        <img class="our_team_img" src="img/uploads/alex_finn.jpg" alt="" title="">
-                    </div>
-                    <div class="our_team_position">
-                        <div class="our_team_position_text">Advisor</div>
-                    </div>
-                    <div class="our_team_name">
-                        <div class="our_team_name_text">Alexander Finn</div>
-                    </div>
-                    <div class="our_team_description">
-                        <div class="our_team_description_text">CEO of Rosblack Capital<br>Entrepreneur<br>Bitcoin Enthusiast</div>
-                    </div>
-                    <div class="social_networks_wrapper">
-                        <a class="social_networks_a tw" href="https://twitter.com/ag_finn" target="_blank"></a>
-                        <a class="social_networks_a fb" href="https://www.facebook.com/alexgfinn" target="_blank"></a>
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/marie-noel-nsana-120a433/" target="_blank"></a>
                     </div>
                 </div>
                 <div class="our_team_item">
@@ -43,7 +25,7 @@
                         <img class="our_team_img" src="img/uploads/hans_choi.jpg" alt="" title="">
                     </div>
                     <div class="our_team_position">
-                        <div class="our_team_position_text">Advisor</div>
+                        <div class="our_team_position_text">Technical Advisor</div>
                     </div>
                     <div class="our_team_name">
                         <div class="our_team_name_text">Hans Choi</div>
@@ -57,10 +39,27 @@
                 </div>
                 <div class="our_team_item">
                     <div class="our_team_img_wrapper">
+                        <img class="our_team_img" src="img/uploads/edward_luo.jpg" alt="" title="">
+                    </div>
+                    <div class="our_team_position">
+                        <div class="our_team_position_text">Financial Advisor</div>
+                    </div>
+                    <div class="our_team_name">
+                        <div class="our_team_name_text">Edward Luo</div>
+                    </div>
+                    <div class="our_team_description">
+                        <div class="our_team_description_text">Master’s in Accounting and Bachelor’s in Finance from New York University<br>Six years in financial industry<br>Expert in BSA/AML and OFAC Sanctions</div>
+                    </div>
+                    <div class="social_networks_wrapper">
+                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/edward-luo/" target="_blank"></a>
+                    </div>
+                </div>
+                <div class="our_team_item">
+                    <div class="our_team_img_wrapper">
                         <img class="our_team_img" src="img/uploads/yan_stolyar.jpg" alt="" title="">
                     </div>
                     <div class="our_team_position">
-                        <div class="our_team_position_text">Advisor</div>
+                        <div class="our_team_position_text">Legal Advisor</div>
                     </div>
                     <div class="our_team_name">
                         <div class="our_team_name_text">Yan Stolyar</div>
@@ -77,36 +76,37 @@
             <div class="our_team_string our_team_string2">
                 <div class="our_team_item">
                     <div class="our_team_img_wrapper">
-                        <img class="our_team_img" src="img/uploads/marie_noel.jpg" alt="" title="">
+                        <img class="our_team_img" src="img/uploads/sls.png" alt="" title="" onclick="window.open('https://bitcointalk.org/index.php?topic=1317448.0')">
                     </div>
                     <div class="our_team_position">
-                        <div class="our_team_position_text">Advisor</div>
+                        <div class="our_team_position_text">Technical Advisor</div>
                     </div>
                     <div class="our_team_name">
-                        <div class="our_team_name_text">Marie-Noel Nsana</div>
+                        <div class="our_team_name_text">Kushed</div>
                     </div>
                     <div class="our_team_description">
-                        <div class="our_team_description_text">15 years in the Banking and Financial industry<br>Anti-Money Laundering Certified<br>Bitcoin &amp; Cryptocurrency Professional (C4 cert)</div>
+                        <div class="our_team_description_text" onclick="window.open('https://bitcointalk.org/index.php?topic=1317448.0')"><span class="clickable">The founder of SaluS (SLS)</span></div>
                     </div>
                     <div class="social_networks_wrapper">
-                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/marie-noel-nsana-120a433/" target="_blank"></a>
+                        <a class="social_networks_a tw" href="https://twitter.com/Kushed_Crypto" target="_blank"></a>
                     </div>
                 </div>
                 <div class="our_team_item">
                     <div class="our_team_img_wrapper">
-                        <img class="our_team_img" src="img/uploads/edward_luo.jpg" alt="" title="">
+                        <img class="our_team_img" src="img/uploads/alex_finn.jpg" alt="" title="">
                     </div>
                     <div class="our_team_position">
-                        <div class="our_team_position_text">Advisor</div>
+                        <div class="our_team_position_text">Commercial Advisor</div>
                     </div>
                     <div class="our_team_name">
-                        <div class="our_team_name_text">Edward Luo</div>
+                        <div class="our_team_name_text">Alexander Finn</div>
                     </div>
                     <div class="our_team_description">
-                        <div class="our_team_description_text">Master’s in Accounting and Bachelor’s in Finance from New York University<br>Six years in financial industry<br>Expert in BSA/AML and OFAC Sanctions</div>
+                        <div class="our_team_description_text">CEO of Rosblack Capital<br>Entrepreneur<br>Bitcoin Enthusiast</div>
                     </div>
                     <div class="social_networks_wrapper">
-                        <a class="social_networks_a ln" href="https://www.linkedin.com/in/edward-luo/" target="_blank"></a>
+                        <a class="social_networks_a tw" href="https://twitter.com/ag_finn" target="_blank"></a>
+                        <a class="social_networks_a fb" href="https://www.facebook.com/alexgfinn" target="_blank"></a>
                     </div>
                 </div>
             </div>
