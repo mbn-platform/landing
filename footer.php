baffe151b00589c80fdee85295812c573d0899e2
Vyacheslav Mychkin <vyacheslav.mychkin@gmail.com>
2018-03-06 15:20:35 +0300

    added telegram button

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 
 </div><!-- page wrapper -->
 </div><!-- page wrapper full -->
+<div class="telegram__icon-wrapper  _top">
+  <a href="https://t.me/membrana_io" class="telegram__icon" target="_blank"></a>
+</div>
 <div class="footer_wrapper">
     <div class="footer_triangles_wrapper">
         <div class="footer_triangles"></div>    
